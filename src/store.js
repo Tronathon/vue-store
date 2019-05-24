35018140eeee26412d0e0ab1b5a2b033fcf3aab1
Anthony Mooney <anthony@nixondesign.com>
2019-05-24 15:45:39 +0100

    second commit

--- src/store.js
+++ src/store.js
@@ -83,9 +83,42 @@ export default new Vuex.Store({
     ]
   },
   mutations: {
-    
+    addToCart(state, payload) {
+      state.cart.push(Number(payload))
+    },
+    removeFromCart (state, payload) {
+      let indexToDelete = state.cart.indexOf( Number(payload) );
+      state.cart.splice(indexToDelete, 1)
+    },
+    decrementProductInventory(state, payload) {
+      let product = state.products.find(product => product.id === Number(payload))
+      product.quantity--;
+    },
+    incrementProductInventory(state, payload) {
+      let product = state.products.find(product => product.id === Number(payload))
+      product.quantity++;
+    }
   },
   actions: {
-    
+    addToCart({ commit }, payload) {
+      commit('addToCart', payload)
+      commit('decrementProductInventory', payload)
+    },
+    removeFromCart({ commit }, payload) {
+      commit('removeFromCart', payload)
+      commit('incrementProductInventory', payload)
+    }
+  },
+  getters: {
+    product: (state) => (id) => {
+      return state.products.filter(p => p.id === Number(id))[0]
+    },
+    cartItems: (state) => {
+      return state.cart.map(
+        itemId => state.products.find(
+          product => product.id === itemId
+        )
+      )
+    }
   }
 });
